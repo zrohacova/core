7fa13893100912655ed3db6849c2975b76231e82
Thushini <kobala@student.chalmers.se>
2023-11-15 21:31:14 +0000

    Added logic to fetch playlist based on day

--- homeassistant/components/spotify/recommendation_handling.py
+++ homeassistant/components/spotify/recommendation_handling.py
@@ -1,48 +1,46 @@
-from spotipy import Spotify  # noqa: D100
-from typing import Any
+"""Provides handling for Spotify playlist recommendations in Home Assistant based on weather conditions and dates."""
+from typing import Any, Optional
+
+from spotipy import Spotify
+
 from homeassistant.core import HomeAssistant
 
 BROWSE_LIMIT = 48
 
-class RecommendationHandler:  # noqa: D101
 
-    _instance = None
+class RecommendationHandler:
+    """A singleton class to handle Spotify playlist recommendations in Home Assistant.
 
-    _last_weather_search_string = ""
-    _last_api_call_result_weather = []
+    This class manages recommendations based on weather conditions and dates,
+    utilizing the Spotify API to fetch relevant playlists.
+    """
 
+    _instance: Optional["RecommendationHandler"] = None
+    _last_weather_search_string: str = ""
+    _last_api_call_result_weather: list[Any] = []
     _media: dict[str, Any] | None = None
 
-    def __new__(cls): # singleton pattern
+    def __new__(cls) -> "RecommendationHandler":  # singleton pattern
+        """Create a new instance of RecommendationHandler or return the existing instance."""
         if not cls._instance:
-            cls._instance = super(RecommendationHandler, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
         return cls._instance
 
-    def handling_weather_recommendations(  # noqa: D102
-        self, hass: HomeAssistant, spotify: Spotify
-    ):  # noqa: D102
+    def handling_weather_recommendations(self, hass: HomeAssistant, spotify: Spotify):
+        """Fetch Spotify playlists for weather-based recommendations.
 
+        The method uses current weather conditions to generate a search string for querying Spotify
+        to find relevant playlists. Caches the last API call result for efficiency.
+        """
         items = []
         media: dict[str, Any] | None = None
 
-        '''weather_entity_id = "weather.home"
-        weather_state = hass.states.get(weather_entity_id)
-
-        if (
-            weather_state is not None
-            and "temperature" in weather_state.attributes
-            and "forecast" in weather_state.attributes
-        ):
-            current_temperature = weather_state.attributes["temperature"]  # noqa: F841
-            condition = weather_state.attributes["forecast"][0][  # noqa: F841
-                "condition"
-            ]  # noqa: F841
-            # send current_temperature and condition to search_string_generator and get search string
-           '''
         current_weather_search_string = "cold rain"
 
         if self._last_weather_search_string != current_weather_search_string:
-            if media := spotify.search(q=current_weather_search_string, type="playlist", limit=BROWSE_LIMIT):
+            if media := spotify.search(
+                q=current_weather_search_string, type="playlist", limit=BROWSE_LIMIT
+            ):
                 items = media.get("playlists", {}).get("items", [])
 
                 self._last_api_call_result_weather = items
@@ -51,5 +49,22 @@ class RecommendationHandler:  # noqa: D101
         else:
             items = self._last_api_call_result_weather
             media = self._media
+
+        return media, items
+
+    def handling_date_recommendations(self, hass: HomeAssistant, spotify: Spotify):
+        """Fetch Spotify playlists for date-based recommendations using a predefined query."""
+        items = []
+        media: dict[str, Any] | None = None
+
+        # Define the search query for current date
+        # FIX: Currently, search_query is a static value need to modify this once the method to fetch this value is developed
+        current_date_search_string = "winter"
+
+        # Perform a Spotify playlist search with the defined query and type.
+        if media := spotify.search(
+            q=current_date_search_string, type="playlist", limit=BROWSE_LIMIT
+        ):
+            items = media.get("playlists", {}).get("items", [])
 
         return media, items
